86268646982ca48dd170e4a00ff2790e5fb73a04
mehulwarade <mwarade@deakin.edu.au>
2017-09-01 10:52:53 +1000

    Add files via upload

---
+++ Motion_sensor_and_LED.ino
@@ -0,0 +1,22 @@
+void setup() {
+  // put your setup code here, to run once:
+   pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(9600); // open serial port, set the baud rate as 9600 bps
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+        if (digitalRead(6) == HIGH) {
+               Serial.println("Active");
+               delay(1000);
+               digitalWrite(LED_BUILTIN, HIGH);                  
+           
+                 
+         
+         }
+        else {
+          Serial.println("Inactive");
+          digitalWrite(LED_BUILTIN, LOW);
+         }
+        delay(1000);
+}
